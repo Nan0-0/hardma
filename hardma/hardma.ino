a07072ff789803259d70652c2e74b28f584fbef1
Nan0-0 <145148559+Nan0-0@users.noreply.github.com>
2024-04-23 10:07:48 -0700

    Blink prgm + getting started on tama

--- hardma/hardma.ino
+++ hardma/hardma.ino
@@ -1,9 +1,40 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Arduino.h>
 
+const int INITIAL_CLEANLINESS_LEVEL = 10;
+const int POOP_CLEANLINESS_DECREASE = 2;
+const int UNCLEANED_POOP_THRESHOLD_1 = 5;
+const int UNCLEANED_POOP_THRESHOLD_2 = 10;
+const int CLEANLINESS_THRESHOLD = 5;
+
+void setup () {
+
+}
+
+void loop () {
+  checkCleanlisness
+}
+
+
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  static int count = 0; 
 
+  digitalWrite(LED_BUILTIN, HIGH); 
+  delay(100); 
+  digitalWrite(LED_BUILTIN, LOW); 
+  delay(100); 
+
+  count++; 
+
+  if (count >= 100) {
+    while (true) {
+      
+    }
+  }
 }
+
+
